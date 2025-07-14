b65f42791d98f950c42c0a59dc141565517ae5b4
AlexsandroRios <gamersnacke@gmail.com>
2025-07-14 17:29:14 -0300

    Add: Criação inicial do projeto com regra de seleção de candidatos

---
+++ processo-seletivo/src/main/java/candidatura/Main.java
@@ -0,0 +1,21 @@
+package candidatura;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Processo Seletivo");
+        analisarCandidato(1820.65);
+        analisarCandidato(3000);
+        analisarCandidato(2000);
+    }
+
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if(salarioBase == salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }else{
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+}
